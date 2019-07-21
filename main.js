73e2fe4611e7a588d3e216b6ec7f6d30f2ff3cb0
Rawabi Okour <rawabi.f.okour@gmail.com>
2019-07-21 10:20:15 +0300

    Finshed the assment week6

--- main.js
+++ main.js
@@ -37,12 +37,25 @@ Ex: addKeyPowerIndex(arrOfObj2)
   {2: 4, c: 'duck'}
 ]   
 */
-
+var arrOfObj1 = [
+  { a: 12 },
+  { b: 5 },
+  { c: 16 },
+  { d: 4 },
+  { e: 3 }
+];
 function addKeyPowerIndex() {
-  // WRITE YOUR CODE UNDER THIS LINE  
-
+  // WRITE YOUR CODE UNDER THIS LINE 
+  var ar=[];
+  var result=arrOfObj1.forEach((elem,index)=>{
+    for (var key in elem) {
+      ar.push(elem[key]=index*index);
+  }
+  // add each index object contan two keys the first key index:index*index  + privious object
+  return ar;}) 
+return result;
 }
-
+console.log(addKeyPowerIndex(arrOfObj1));
 
 
 
@@ -63,12 +76,15 @@ Ex: decreseBy(arrOfnum1,10)
 Ex: decreseBy(arrOfNum1,-6)
 => [83, 11, 39]
 */
+var arrOfNum1 = [77,5,33];
 
-function decreseBy() {
+function decreseBy(arrOfNum1,number) {
   // WRITE YOUR CODE UNDER THIS LINE 
-
+var result=arrOfNum1.map(num=> num - number);
+return result;
 }
-
+console.log(decreseBy(arrOfNum1,10));
+console.log(decreseBy(arrOfNum1,-6));
 
 
 
@@ -101,11 +117,25 @@ Ex: nameLongerThan(arrOfObj4,6)
   { name: "elizabeth"}
 ]
 */
-
-function nameLongerThan() {
-  // WRITE YOUR CODE UNDER THIS LINE         
+var arrOfObj3 = [
+  { name: "alex" },
+  { name: "mercer"},
+  { name: "alice" },
+  { name: "zaheer"},
+  { name: "elizabeth"}
+];
+function nameLongerThan(arrOfObj3,number) {
+  // WRITE YOUR CODE UNDER THIS LINE      
+  var result = arrOfObj3.filter(ob=>{
+    for (var key in ob) {
+     if (ob[key].length>number) {
+       return ob[key];
+     }
+    }
+  })  
+  return result;
 }
-
+console.log(nameLongerThan(arrOfObj3,4));
 
 
 
@@ -132,10 +162,22 @@ Ex: avgLength(arrOfObj4,"name")
 Ex: avgLength(arrOfObj4,"food")
 => 6.8
 */
+var arrOfObj4 = [
+  { name: "alex", food: "fried chiken" },
+  { name: "mercer", food: "pizaa" },
+  { name: "alice", food: "burger" },
+  { name: "zaheer", food: "hot dog" },
+  { name: "elizabeth", food: "eggs" }
+];
+function avgLength(arrOfObj4,key) {
+  // WRITE YOUR CODE UNDER THIS LINE   
+  var result= arrOfObj4.reduce((acc,elem)=>{
+    for(var key in elem) {
+    return acc + elem[key].length;
+  }
 
-function avgLength() {
-  // WRITE YOUR CODE UNDER THIS LINE         
-
+},0)  ;    
+return result/arrOfObj4.length;
 }
-
+console.log(avgLength(arrOfObj4,"name"));
 // Good luck :)
